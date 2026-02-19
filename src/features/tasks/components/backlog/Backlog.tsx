b1a83d233ac3ede4ed95763a34c9cb3c485cbb02
Salim <doshka23@gmail.com>
2026-02-19 19:40:10 +0100

    added: create task form

--- src/features/tasks/components/backlog/Backlog.tsx
+++ src/features/tasks/components/backlog/Backlog.tsx
@@ -1,11 +1,36 @@
+import { useState } from 'react';
 import './_backlog.scss';
 import { BiPlus } from "react-icons/bi";
+import { CreateTaskForm } from '../forms/CreateTaskForm';
 
 export const Backlog = () => {
+    const [isCreateTask, setIsCreateTask] = useState<boolean>(false);
+    const [message, setMessage] = useState("Drag the item and drop it in the box.");
+
+    const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+        setMessage("Dragging...");
+        e.dataTransfer.setData("text/plain", "hello"); // any payload
+    };
+
+    const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault(); // ✅ REQUIRED so onDrop can fire
+    };
+
+    const onDrop = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        const data = e.dataTransfer.getData("text/plain");
+        setMessage(`Dropped! payload="${data}"`);
+        console.log("DROP fired ✅ payload:", data);
+    };
+
+    
     return(
         <section className="backlog-container">
             <div className='backlog'>
-               <section className='backlog__header'>
+               <section 
+                draggable
+                onDragStart={onDragStart}
+                className='backlog__header'>
                     <div>
                         backlog (0 item)
                     </div>
@@ -16,9 +41,19 @@ export const Backlog = () => {
                 <section className='backlog__content' >
                     your backlog is empty
                 </section>
-                <section className='backlog__footer'>
-                    <span><BiPlus /></span>
-                    create work Item
+                <section
+                    onDragOver={onDragOver}
+                    onDrop={onDrop}
+                    className='backlog__footer'>
+                   
+                    {
+                        isCreateTask  ? <CreateTaskForm></CreateTaskForm>  : 
+                       <div onClick={() => setIsCreateTask(true)}>
+                            <span><BiPlus /></span>
+                            create work Item
+                       </div>
+                    }
+                    
                 </section>
             </div>
         </section>
