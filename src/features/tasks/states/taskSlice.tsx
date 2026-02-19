27c5c491bb1ff88111ca0db7ef292c23497eec24
Salim <doshka23@gmail.com>
2026-02-19 19:42:29 +0100

    added: task slice instance

---
+++ src/features/tasks/states/taskSlice.tsx
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const  initialState = {};
+const createTaskSlice = createSlice({
+    name: 'taskSlice',
+     initialState,
+    reducers: {}
+})
+
+export default createTaskSlice.reducer;
