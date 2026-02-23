18d60f4e038d615cb4eb0c730b3d6524eed58652
Salim <doshka23@gmail.com>
2026-02-23 15:03:42 +0100

    built bring edit sprint modal from list

--- src/features/tasks/components/edit-sprint-date/EditSprintDate.tsx
+++ src/features/tasks/components/edit-sprint-date/EditSprintDate.tsx
@@ -4,37 +4,47 @@ import { EditSprintForm } from "../forms/EditSprintForm";
 import type { Sprint } from "../../model/sprint.model";
 import { FaRegEdit } from "react-icons/fa";
 
-export const EditSprintDate = (sprint: Sprint) => {
-    const [isEditSprintOpen, setEditSprintOpen] = useState<boolean>(false);   
-  
-    const onEditSprintDate = (sprint: Sprint) => {
-        console.log(sprint, "hello from edit sprint date");
-        setEditSprintOpen((prev) => !prev);
-    }
-    useEffect(() => {
-    }, [sprint])
-    return (
-       <>
-        {
-            !isEditSprintOpen  ?
-            <section className="edit-sprint-header">
-                <span> 
-                    {
-                    sprint.startDate && sprint.endDate 
-                    ? ` (${new Date(sprint.startDate).toLocaleDateString()} - ${new Date(sprint.endDate).toLocaleDateString()})`
-                    : 
-                    <>
-                        <button onClick={() => onEditSprintDate(sprint)}> 
-                            <span><FaRegEdit/></span> 
-                            add date
-                        </button> 
-                    </>
-                    }
-                </span> 
-            </section>
-            :
-            <EditSprintForm sprint={sprint} setEditSprintOpen={setEditSprintOpen}/>
+export const EditSprintDate = ({
+        sprint, 
+        isEditSprintOpen,
+        setEditSprintOpen,
+    }: { 
+        sprint: Sprint, 
+        isEditSprintOpen: boolean, 
+        setEditSprintOpen: (open: boolean) => void,
+    }) => {
+        //const [isEditSprintOpen, setEditSprintOpen] = useState<boolean>(false);   
+    
+        const onEditSprintDate = (sprint: Sprint) => {
+            console.log(sprint, "hello from edit sprint date");
+            setEditSprintOpen(!isEditSprintOpen);
         }
-       </>
-    )
+        
+        useEffect(() => {
+            
+        }, [sprint])
+        return (
+        <>
+            {
+                !isEditSprintOpen  ?
+                <section className="edit-sprint-header">
+                    <span> 
+                        {
+                        sprint.startDate && sprint.endDate 
+                        ? ` (${new Date(sprint.startDate).toLocaleDateString()} - ${new Date(sprint.endDate).toLocaleDateString()})`
+                        : 
+                        <>
+                            <button onClick={() => onEditSprintDate(sprint)}> 
+                                <span><FaRegEdit/></span> 
+                                add date
+                            </button> 
+                        </>
+                        }
+                    </span> 
+                </section>
+                :
+                <EditSprintForm sprint={sprint} setEditSprintOpen={setEditSprintOpen}/>
+            }
+        </>
+        )
 }
--- src/features/tasks/components/sprint-header/SprintHeader.tsx
+++ src/features/tasks/components/sprint-header/SprintHeader.tsx
@@ -2,39 +2,47 @@ import './_sprint-header.scss';
 import { OptionsBtn } from "../../../../shared/components/options-btn/OptionsBtn";
 import type { Sprint } from "../../model/sprint.model";
 import { EditSprintDate } from "../edit-sprint-date/EditSprintDate";
+import { useState } from 'react';
 
 export const SprintHeader = ({
-    sprint,
-    isOptionsOpen,
-    setOptionsOpen,
-}: { 
-    sprint: Sprint,
-    isOptionsOpen: string | null, 
-    setOptionsOpen: (sprintId: string | null) => void ,
-}) => {
-    return(
-        <div className='sprint-header'>
-            <div className='sprint-header__title'>
-                Scrum {sprint.name} 
-                <EditSprintDate {...sprint}/>
-                <span> ({sprint.tasks.length} work items) </span>
+        sprint,
+        isOptionsOpen,
+        setOptionsOpen,
+    }: { 
+        sprint: Sprint,
+        isOptionsOpen: string | null, 
+        setOptionsOpen: (sprintId: string | null) => void ,
+    }) => {
+        const [isEditSprintOpen, setEditSprintOpen] = useState<boolean>(false);   
+        return(
+            <div className='sprint-header'>
+                <div className='sprint-header__title'>
+                    Scrum {sprint.name} 
+                    <EditSprintDate 
+                        isEditSprintOpen={isEditSprintOpen} 
+                        setEditSprintOpen={setEditSprintOpen}
+                        sprint={sprint}/>
+                    <span> ({sprint.tasks.length} work items) </span>
+                </div>
+                <div className='sprint-header__actions'>
+                    <button disabled={sprint?.tasks?.length === 0}>start sprint</button>
+                </div>
+                <div 
+                    className='sprint-header__options'>
+                    <OptionsBtn 
+                        item={sprint} 
+                        isOptionsOpen={isOptionsOpen}
+                        setOptionsOpen={setOptionsOpen}
+                    >
+                        <ul className='options-list'>
+                            <li className='options-list__item' onClick={() => setEditSprintOpen((prev) => !prev)}>
+                                edit sprint
+                            </li>
+                    
+                            <li className='options-list__item'>Delete Sprint</li>
+                        </ul>
+                    </OptionsBtn>
+                </div>
             </div>
-            <div className='sprint-header__actions'>
-                <button disabled={sprint?.tasks?.length === 0}>start sprint</button>
-            </div>
-            <div 
-                className='sprint-header__options'>
-                <OptionsBtn 
-                    item={sprint} 
-                    isOptionsOpen={isOptionsOpen}
-                    setOptionsOpen={setOptionsOpen}
-                >
-                    <ul className='options-list'>
-                        <li className='options-list__item'>Edit Sprint</li>
-                        <li className='options-list__item'>Delete Sprint</li>
-                    </ul>
-                </OptionsBtn>
-            </div>
-        </div>
-    )
+        )
 }
