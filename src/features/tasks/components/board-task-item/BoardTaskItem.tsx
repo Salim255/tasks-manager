4563c750d9ec9f34edd46a71420e3e939f64e977
Salim <doshka23@gmail.com>
2026-02-24 02:38:56 +0100

    built: board task item

--- src/features/tasks/components/board-task-item/BoardTaskItem.tsx
+++ src/features/tasks/components/board-task-item/BoardTaskItem.tsx
@@ -1,37 +1,39 @@
+import './_board-task-item.scss';
 import { useState } from "react";
 import { IoCheckboxOutline } from "react-icons/io5";
 import { OptionsBtn } from "../../../../shared/components/options-btn/OptionsBtn";
 import type { TaskItemProps } from "../task-item/TaskItem";
+import { GoPerson } from "react-icons/go";
 
 export const BoardTaskItem =  ({ task, ...props }: TaskItemProps) => {
     const [isOptionsOpen, setOptionsOpen ] = useState<string | null>(null);
     return (
-        <div className="task-item" {...props} >
-            <section className='task-item__content'>
-                <div className='task-item__checkbox'>
-                    <IoCheckboxOutline/>
-                </div>
-                <div className='task-item__title'>
+        <div className="board-task-item" {...props} >
+            <section className='board-task-item__content'>
+                <div className='board-task-item__title'>
                     {task.title}  
                 </div>
+                <div className='board-task-item__actions'>
+                    <OptionsBtn 
+                        item={task} 
+                        isOptionsOpen={isOptionsOpen}
+                        setOptionsOpen={setOptionsOpen}
+                    >
+                        <ul className='options-list'>
+                            <li className='options-list__item'>Edit Task</li>
+                            <li className='options-list__item'>Delete Task</li>
+                        </ul>
+                    </OptionsBtn>
+                </div>
             </section>
-            <section className='task-item__status'>
-                {task.status}
-            </section>
-            <section className='task-item__assignee'>
-                {task.assigneeId}
-            </section>
-            <section className='task-item__actions'>
-                <OptionsBtn 
-                    item={task} 
-                    isOptionsOpen={isOptionsOpen}
-                    setOptionsOpen={setOptionsOpen}
-                >
-                    <ul className='options-list'>
-                        <li className='options-list__item'>Edit Task</li>
-                        <li className='options-list__item'>Delete Task</li>
-                    </ul>
-                </OptionsBtn>
+            <section className='board-task-item__footer'>
+                <div className='board-task-item__checkbox'>
+                    <IoCheckboxOutline className="board-task-item__checkbox-icon"/> <span> sprint name </span>
+                </div>
+                <div className='board-task-item__assignee'>
+                    {/* {task.assigneeId} */}
+                    <GoPerson/>
+                </div>
             </section>
         </div>
     )
