b29e29ea5f79ef0aedee8bdac37a61e1ce858a93
Salim <doshka23@gmail.com>
2026-03-05 20:07:22 +0100

    added: database module

--- servers/server/src/app.module.ts
+++ servers/server/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ProfileModule } from './modules/profile/profile.module';
+import { ProjectModule } from './modules/project/project.module';
+import { SprintModule } from './modules/sprint/sprint.module';
+import { TaskModule } from './modules/task/task.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TaskModule,
+    SprintModule,
+    ProjectModule,
+    ProfileModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- servers/server/src/main.ts
+++ servers/server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT ?? 3000;
+  await app.listen(PORT, () => {
+    console.log(`Task manager's, server running on port number: ✅`, `${PORT}`);
+  });
 }
-bootstrap();
+
+bootstrap().catch(() => {
+  process.exit(1);
+});
