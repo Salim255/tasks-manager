4990fda47211fb536b69dd9288de0e181099b097
Salim <doshka23@gmail.com>
2026-03-06 02:15:35 +0100

    added: project controller

---
+++ servers/server/src/modules/project/controller/project.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('Projects')
+export class ProjectController {}
--- servers/server/src/modules/project/project.module.ts
+++ servers/server/src/modules/project/project.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProjectService } from './service/project.service';
 import { ProjectRepository } from './repository/project.repository';
 import { DatabaseModule } from 'src/database/database.module';
+import { ProjectController } from './controller/project.controller';
 
 @Module({
   imports: [DatabaseModule],
   providers: [ProjectService, ProjectRepository],
+  controllers: [ProjectController],
 })
 export class ProjectModule {}
