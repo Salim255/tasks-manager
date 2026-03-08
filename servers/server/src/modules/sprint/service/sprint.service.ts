e8584412558c77666f7fdafa939081480a60c12f
Salim <doshka23@gmail.com>
2026-03-09 00:46:16 +0100

    built: create sprint query

--- servers/server/src/modules/sprint/service/sprint.service.ts
+++ servers/server/src/modules/sprint/service/sprint.service.ts
@@ -1,9 +1,54 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { SPRINT_REPOSITORY } from 'src/common/constants/constants';
 import { Repository } from 'typeorm';
 import { Sprint } from '../entity/sprint.entity';
 
 @Injectable()
 export class SprintService {
-  constructor(@Inject(SPRINT_REPOSITORY) sprintRepo: Repository<Sprint>) {}
+  private logger = new Logger(SprintService.name);
+
+  constructor(
+    @Inject(SPRINT_REPOSITORY) private sprintRepo: Repository<Sprint>,
+  ) {}
+
+  async createSprint(payload: { projectId: string }): Promise<Sprint> {
+    try {
+      const values = [payload.projectId];
+      const query = `
+        WITH sprint_counter (
+          SELECT COUNT(*) AS total
+            FROM sprints AS sp
+              WHERE sp."projectId" = $1
+        ),
+      
+        inserted (
+          INSERT INTO sprints (name, projectId)
+            SELECT CONCAT('print', sprint_counter.total), $1
+              RETURNING *;
+        )
+        
+        SELECT * FROM inserted;
+      `;
+      const sprint: Sprint = await this.sprintRepo.query(query, values);
+      return sprint;
+    } catch (error) {
+      this.logger.error('Error to create sprint');
+      throw error;
+    }
+  }
+
+  async countProjectSprints(projectId: string): Promise<number> {
+    try {
+      const query = `
+        SELECT COUNT(*) AS totalSprint
+          FROM sprints AS spt
+            WHERE spt."projectId" = $1
+        `;
+      const counter: number = await this.sprintRepo.query(query, [projectId]);
+      return counter;
+    } catch (error) {
+      this.logger.error('Error counting sprints by project');
+      throw error;
+    }
+  }
 }
