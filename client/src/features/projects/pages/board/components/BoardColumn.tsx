4bc51e8b7c8d210ce81ba88c81af742ebcf2c9a5
Salim <doshka23@gmail.com>
2026-03-12 22:33:52 +0100

    refactored: board

--- client/src/features/projects/pages/board/components/BoardColumn.tsx
+++ client/src/features/projects/pages/board/components/BoardColumn.tsx
@@ -1,15 +1,13 @@
 import { DiScrum } from "react-icons/di";
 import { BoardTaskItem } from "../../../components/board-task-item/BoardTaskItem";
-import type { Sprint } from "../../../models/sprint.model";
 import type { Task, TaskStatus } from "../../../models/task.model";
 import { NavLink, useParams } from "react-router-dom";
-import { useMemo, type DragEvent } from "react";
+import { type DragEvent } from "react";
 
 export interface BoardColumnProps {
   title: string;
   status: TaskStatus;
   tasks: Task[];
-  sprints: Sprint [];
   onDragStart: (e: DragEvent, task: Task) => void;
   onDrop: (e: DragEvent, status: TaskStatus) => void;
   onDragOver: (e: DragEvent) => void;
@@ -18,22 +16,16 @@ export interface BoardColumnProps {
 export const BoardColumn = ({
         title,
         status,
-        sprints,
         tasks,
         onDragStart,
         onDrop,
         onDragOver
     }: BoardColumnProps,
     ) => {
-    const activeSprintIds = useMemo(() => {
-            return new Set(sprints
-                .filter((sprint) => sprint.status === 'active')
-                .map((sprint) => sprint.id)
-            );
-        }, [sprints]);
+  
     
     const { projectId }  = useParams<string>() ;
-    const countTasksByStatus = (status: TaskStatus): number | null => {
+  /*   const countTasksByStatus = (status: TaskStatus): number | null => {
         const counter = tasks
             ?.filter(
                 (task: Task) => 
@@ -42,46 +34,33 @@ export const BoardColumn = ({
             ).length || null;
         return counter ;
     }
-    
+ */
     return   <div className={`board-column ${status}`}
                 onDrop={(e) => onDrop(e, status)}
                 onDragOver={onDragOver}
             >
                 <div className='todo__header'>
-                    {title} { countTasksByStatus(status) }
+                    {title} { tasks.length }
                 </div>
 
                 {
-                    sprints
-                    ?.filter(
-                        (spt: Sprint) => spt.status === 'active'
-                    )?.length 
+                    tasks.length 
                     ? 
-                        sprints.
-                        filter((sprint) => sprint.status === 'active')
-                        .map((sprint) => {
-                            
-                            return <div key={sprint.id} > 
-                                {
-                                    tasks.map((task: Task) => {
-                                    return (
-                                        (
-                                            task.status === status
-                                            && 
-                                            task.sprintId === sprint.id
-                                        ) 
-                                        &&
-                                        <BoardTaskItem 
-                                            key={task.id} 
-                                            task={task} 
-                                            draggable
-                                            onDragStart={(e) => onDragStart(e, task)}
-                                            /> 
-                                        )
-                                    })
-                                }
-                            </div>
-                        })
+                        <div> 
+                            {
+                                tasks.map((task: Task) => {
+                                return (
+                                    <BoardTaskItem 
+                                        key={task.id} 
+                                        task={task} 
+                                        draggable
+                                        onDragStart={(e) => onDragStart(e, task)}
+                                        /> 
+                                    )
+                                })
+                            }
+                        </div>
+                        
                     : <div className='todo__empty'>
                         <DiScrum className='icon'/>
                         <h3>
