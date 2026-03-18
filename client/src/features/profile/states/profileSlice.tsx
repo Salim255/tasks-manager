bdeeb7171b4d00d4a0f837d2412ef20b39d8a3b2
Salim <doshka23@gmail.com>
2026-03-19 00:59:28 +0100

    built:profileSlice

---
+++ client/src/features/profile/states/profileSlice.tsx
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createProfileHttp } from "../http/profileHttp";
+import { type Profile } from "../model/profile.model";
+
+type InitiateState = {
+    profile?: Profile;
+    isCreating: boolean;
+    isLoading: boolean;
+}
+
+const initiateState: InitiateState = {
+    isCreating: false,
+    isLoading: false
+};
+
+const profileSlice = createSlice({
+    name: 'profileSlice',
+    initialState: initiateState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(createProfileHttp.pending, (state) => {
+            state.isCreating = true;
+        })
+        .addCase(createProfileHttp.fulfilled, (state, action) => {
+            const { profile } = action.payload.profile;
+            state.profile = profile;
+            state.isCreating = false;
+        })
+        .addCase(createProfileHttp.rejected, (state) => {
+            state.isCreating = false;
+        })
+    },
+
+    } 
+)
