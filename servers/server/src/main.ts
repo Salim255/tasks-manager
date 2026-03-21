32dd2deb3018de9d5abfc16a8b2fc7f7434112ff
Salim <doshka23@gmail.com>
2026-03-21 13:05:20 +0100

    bug: corrected: pop bug

--- servers/server/src/main.ts
+++ servers/server/src/main.ts
@@ -41,7 +41,7 @@ async function bootstrap(): Promise<void> {
   const configService = app.get(ConfigService);
   app.useGlobalFilters(new GlobalExceptionFilter(configService));
 
-  await app.listen(PORT);
+  await app.listen(PORT, '127.0.0.1');
   logger.log(`Task manager's, server running... on port number: ✅`, `${PORT}`);
 }
 
