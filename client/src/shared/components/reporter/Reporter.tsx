3479d9d927c83cdc8cd4e669e2f2baa515ec35ee
Salim <doshka23@gmail.com>
2026-03-25 02:19:40 +0100

    removed: fail source

--- client/src/shared/components/reporter/Reporter.tsx
+++ client/src/shared/components/reporter/Reporter.tsx
@@ -3,6 +3,9 @@ import { useProfileSelector } from "../../../features/profile/states/profileSele
 export const Reporter = ({ reporterId }:{ reporterId: string }) => {
     const {profile} = useProfileSelector();
     const getInitials = () => {
+        if (profile?.id === reporterId){
+            return profile?.firstName?.trim()[0].concat(profile?.lastName?.trim()[0]);
+        }
         return profile?.firstName?.trim()[0].concat(profile?.lastName?.trim()[0]);
     }
     return <div>
