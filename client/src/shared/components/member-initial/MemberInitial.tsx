06ea5956867103647a6e389c864cf5667ac6f814
Salim <doshka23@gmail.com>
2026-04-02 20:29:49 +0200

    built: MemberInitial

---
+++ client/src/shared/components/member-initial/MemberInitial.tsx
@@ -0,0 +1,18 @@
+import { GoPerson } from "react-icons/go";
+import { getInitialsFromProfile } from "../../utils/methods";
+import type { Profile } from "../../../features/profile/model/profile.model";
+
+export const MemberInitial = ({ profile }: {profile: Profile}) => {
+    return(
+        <div className="member-initial">
+            { 
+                profile
+                ? 
+                <span className='member-initial__initial'> 
+                    { getInitialsFromProfile(profile) } 
+                </span> 
+                : <GoPerson className='member-initial__icon'/> 
+            }
+        </div>
+    )
+}
