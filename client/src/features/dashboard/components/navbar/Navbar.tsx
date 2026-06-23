0669420a0e6e301d809ccab90994678630a9e5bb
Salim <doshka23@gmail.com>
2026-06-23 15:49:06 +0200

    built: user menu

--- client/src/features/dashboard/components/navbar/Navbar.tsx
+++ client/src/features/dashboard/components/navbar/Navbar.tsx
@@ -1,73 +1,44 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './_navbar.scss';
-import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
-import { useEffect, useRef, useState } from 'react';
+import { FaAlignLeft } from 'react-icons/fa';
+import { useEffect } from 'react';
 import { updateSidebarOpenState } from '../../states/dashboardSlice';
 import { type AppDispatch, type RootState } from '../../../../redux/store';
-import { NavLink } from 'react-router-dom';
 import { useProfileSelector } from '../../../profile/states/profileSelectors';
-import { useClickOutside } from '../../../../shared/hooks/useClickOutside';
-import { logout } from '../../../auth/states/logout';
+import { UserMenu } from '../user-menu/UserMenu';
 
 
 export const Navbar = () => {
-    const ref = useRef<HTMLDivElement>(null!);
-    const [showLogout, setShowLogout] = useState(false);
     const { dashboard } = useSelector((store: RootState) => store);
     const { profile } = useProfileSelector();
     const dispatch = useDispatch<AppDispatch>();
-
-    const handleLogout = () => {
-        logout(dispatch);
-    }
 
     const toggle =  () =>  {
       dispatch(updateSidebarOpenState())
     }
 
-    const { register, unregister } = useClickOutside();
     useEffect(() => {
-        if (ref.current) {
-          register(ref, () => setShowLogout(false));
-        }
-        return () => unregister(ref);
-    }, [dashboard, profile, register, unregister, showLogout]);
+
+    }, [dashboard, profile]);
     
     return (
-      <nav className='navbar'>
-        <div className='navbar__nav-center'>
-            <button type='button' className='toggle-btn' onClick={toggle}>
-                <FaAlignLeft />
+     <nav className="navbar">
+        <div className="navbar__container">
+
+            <button
+            type="button"
+            className="navbar__toggle"
+            onClick={toggle}
+            >
+            <FaAlignLeft />
             </button>
-            <div>
-                <h4 className='logo-text'>FlowBoard</h4>
-            </div>
-            <div className='btn-container' ref={ref}>
-                <button
-                    type='button'
-                    className='btn'
-                    onClick={() => (profile ? setShowLogout(!showLogout) : handleLogout())}>
-                    <FaUserCircle />
-                    { profile ? profile?.firstName: 'logout'}
-                    {profile && <FaCaretDown />}
-                </button>
-                <div  className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
-                    <button
-                    type='button'
-                    className='dropdown-btn'
-                    onClick={handleLogout}>
-                        logout
-                    </button>
-                    <NavLink 
-                        to={"/profile"}
-                        onClick={() => setShowLogout(!showLogout)}
-                        className='dropdown-btn btn-container__user-profile'>
-                        profile
-                    </NavLink>
-                </div>
-            </div>
+
+            <h1 className="navbar__logo">
+            FlowBoard
+            </h1>
+            <UserMenu profile={profile}/>
         </div>
-      </nav>
+        </nav>
     )
 }
 
---
+++ client/src/features/dashboard/components/user-menu/UserMenu.tsx
@@ -0,0 +1,84 @@
+import './_user-menu.scss';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../../auth/states/logout';
+import type { AppDispatch } from '../../../../redux/store';
+import { useEffect, useRef, useState } from 'react';
+import type { Profile } from '../../../profile/model/profile.model';
+import { FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import { useClickOutside } from '../../../../shared/hooks/useClickOutside';
+
+export const UserMenu = ({ profile }:{ profile: Profile | undefined }) => {
+  const [showLogout, setShowLogout] = useState(false);
+  const ref = useRef<HTMLDivElement>(null!);
+  const dispatch = useDispatch<AppDispatch>();
+
+
+   const { register, unregister } = useClickOutside();
+
+  const handleLogout = () => {
+          logout(dispatch);
+      }
+
+  
+  useEffect(() => {
+      if (ref.current) {
+        register(ref, () => setShowLogout(false));
+      }
+      return () => unregister(ref);
+  }, [ profile, register, unregister, showLogout]);
+
+  return (
+    <div className="user-menu" ref={ref}>
+      <button
+        type="button"
+        className="user-menu__trigger"
+        onClick={() =>
+          profile
+            ? setShowLogout(!showLogout)
+            : handleLogout()
+        }
+      >
+        <FaUserCircle className="user-menu__icon" />
+
+        <span className="user-menu__name">
+          {profile?.firstName}
+        </span>
+
+        {profile && (
+          <FaCaretDown
+            className={`user-menu__caret ${
+              showLogout
+                ? "user-menu__caret--open"
+                : ""
+            }`}
+          />
+        )}
+      </button>
+
+      <div
+        className={`user-menu__dropdown ${
+          showLogout
+            ? "user-menu__dropdown--open"
+            : ""
+        }`}
+      >
+        <NavLink
+          to="/profile"
+          className="user-menu__item"
+          onClick={() => setShowLogout(false)}
+        >
+          Profile
+        </NavLink>
+
+        <button
+          type="button"
+          className="user-menu__item"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  )
+}
