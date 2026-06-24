675e2199529953b9fa4f4ae811c6c77f90f308e2
Salim <doshka23@gmail.com>
2026-06-24 12:14:41 +0200

    refactored: PieChart

--- client/src/features/projects/pages/statistics/components/work-status-chart/WorkStatusChart.tsx
+++ client/src/features/projects/pages/statistics/components/work-status-chart/WorkStatusChart.tsx
@@ -8,8 +8,23 @@ export const WorkStatusChart = ({
 }: { 
     circleChartDataPercentage: Record<TaskStatus, { label: TaskStatus; nb: number; value: number }>;
 } ) => {
+
+    const statusColors: Record<TaskStatus, string> = {
+        todo: "hsl(243, 70%, 50%)",
+        in_progress: "hsl(24, 95%, 48%)",
+        done: "hsl(148, 48%, 49%)",
+    };
+
+    const statusData = Object.entries(circleChartDataPercentage).map(
+    ([status, data]) => ({
+        id: status,
+        label: data.label,
+        value: data.nb,
+        color: statusColors[status as TaskStatus],
+    })
+    );
     return <div className="work-status-chart" >
         <h4 className="work-status-chart__title">Work Status</h4>
-        <PieChart /> 
+        <PieChart  data={statusData}/> 
     </div>
 }
--- client/src/shared/kits/pie-chart/PieChart.tsx
+++ client/src/shared/kits/pie-chart/PieChart.tsx
@@ -1,32 +1,23 @@
 import { ResponsivePie } from '@nivo/pie'
 import "./_pie-chart.scss";
 
-export const PieChart = () => {
+
+type PieChartItem = {
+  id: string;
+  label: string;
+  value: number;
+  color: string;
+};
+
+type PieChartProps = {
+  data: PieChartItem[];
+};
 
 
-    const data3 = [
-      {
-        "id": "In Progress",
-        "label": "In Progress",
-        "value": 254,
-        "color": "hsl(24, 95%, 48%)"
-      },
-      {
-        "id": "Done",
-        "label": "Done",
-        "value": 295,
-        "color": "hsl(148, 48%, 49%)"
-      },
-      {
-        "id": "To Do",
-        "label": "TO DO",
-        "value": 478,
-        "color": "hsl(243, 70%, 50%)"
-      }
-    ]
+export const PieChart = ({ data }: PieChartProps) => {
 
     return <ResponsivePie
-        data={data3}
+        data={data}
         colors={{ datum: "data.color" }}
         margin={{ top: 40, right: 40, bottom: 80, left: 80 }}
         innerRadius={0.5}
