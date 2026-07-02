c47576217cf1ab899e1cfab17c3540ed8959f3ad
Salim <doshka23@gmail.com>
2026-07-02 14:19:27 +0200

    built: projectSprintMapper

--- servers/server/src/common/utils/dtoMapper.ts
+++ servers/server/src/common/utils/dtoMapper.ts
@@ -1,3 +1,5 @@
+import { SprintCreatorDto, SprintDto } from "src/modules/sprint/dto/sprint.dto";
+import { Sprint } from "src/modules/sprint/entity/sprint.entity";
 import { TaskAssignerDto, TaskDto, TaskReporterDto } from "src/modules/task/dto/task.dto";
 import { Task } from "src/modules/task/entity/task.entity";
 import { User } from "src/modules/user/entity/user.entity";
@@ -10,12 +12,25 @@ export class DtoMapper {
 
         return {
             ...task,
-            reporter: reporter ? this.taskUserMapper(reporter) : null,
-            assignee:  assignee ? this.taskUserMapper(assignee) : null,
+            reporter: reporter ? this.userMapper(reporter) : null,
+            assignee:  assignee ? this.userMapper(assignee) : null,
         }
     }
 
-    static taskUserMapper = (user: User): TaskReporterDto | TaskAssignerDto | null => {
+    static projectSprintMapper = (sprint: Sprint): SprintDto => {
+        const creator = sprint.creator;
+
+        return {
+            ...sprint,
+            creator: creator ? this.userMapper(creator): null
+        }
+    } 
+    
+    static userMapper = (user: User): 
+        TaskReporterDto 
+        | TaskAssignerDto 
+        | SprintCreatorDto 
+        | null => {
         const profile = user.profile;
         return{
             id: user.id,
