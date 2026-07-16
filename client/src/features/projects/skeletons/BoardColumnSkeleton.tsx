bbcf5403fa13a1a2ffd61a333e07db8d15cfba4e
Salim <doshka23@gmail.com>
2026-07-17 00:33:02 +0200

    built: BoardColumnSkeleton

---
+++ client/src/features/projects/skeletons/BoardColumnSkeleton.tsx
@@ -0,0 +1,42 @@
+import { SkeletonBase } from "../../../../../shared/kits/skeleton-base/SkeletonBase";
+
+export const BoardColumnSkeleton = ({ title }: { title: string }) => {
+  return (
+    <div className="board-column board-column--skeleton">
+
+      {/* HEADER */}
+      <div className="board-column__header">
+
+        <div className="board-column__title-group">
+
+          {/* Indicator */}
+          <SkeletonBase height={16} width={16} viewBox="0 0 16 16">
+            <circle cx="8" cy="8" r="8" />
+          </SkeletonBase>
+
+          {/* Title */}
+          <div className="u-ml-sm">
+            <SkeletonBase height={24} width={120} viewBox="0 0 120 24">
+              <rect x="0" y="0" rx="6" ry="6" width="120" height="20" />
+            </SkeletonBase>
+          </div>
+
+        </div>
+
+        {/* Count */}
+        <SkeletonBase height={20} width={30} viewBox="0 0 30 20">
+          <rect x="0" y="0" rx="6" ry="6" width="30" height="16" />
+        </SkeletonBase>
+
+      </div>
+
+      {/* BODY */}
+      <div className="board-column__body">
+        <BoardTaskItemSkeleton />
+        <BoardTaskItemSkeleton />
+        <BoardTaskItemSkeleton />
+      </div>
+
+    </div>
+  );
+};
