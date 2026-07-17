cf9cc8f6676ff47d2d4df99e547080cbb46d4e0b
Salim <doshka23@gmail.com>
2026-07-17 03:50:03 +0200

    built: AssignedToMePanelSkeleton

---
+++ client/src/features/projects/pages/projects-home/skeletons/AssignedToMePanelSkeleton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const AssignedToMePanelSkeleton: React.FC = () => {
+  return (
+    <section className="projects-home__panel">
+
+      <div className="projects-home__panel-header">
+        <SkeletonBase height={24} width={180} viewBox="0 0 180 24">
+          <rect x="0" y="0" rx="6" ry="6" width="180" height="20" />
+        </SkeletonBase>
+
+        <div className="u-mt-xs">
+          <SkeletonBase height={16} width={240} viewBox="0 0 240 16">
+            <rect x="0" y="0" rx="4" ry="4" width="240" height="12" />
+          </SkeletonBase>
+        </div>
+      </div>
+
+      <div className="projects-home__tasks-list u-mt-md">
+        <TaskItemSkeleton />
+        <TaskItemSkeleton />
+        <TaskItemSkeleton />
+      </div>
+
+    </section>
+  );
+};
