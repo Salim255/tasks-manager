7b1cb00c86bd18cdf9b5bf05c3377685af9cf7c0
Salim <doshka23@gmail.com>
2026-07-17 02:05:51 +0200

    built: ProjectLinkSkeleton

---
+++ client/src/features/dashboard/skeletons/ProjectLinkSkeleton.tsx
@@ -0,0 +1,7 @@
+const ProjectLinkSkeleton = () => (
+  <div className="nav-links__project nav-links__project--skeleton u-mt-sm">
+    <SkeletonBase height={18} width={140} viewBox="0 0 140 18">
+      <rect x="0" y="0" rx="6" ry="6" width="140" height="14" />
+    </SkeletonBase>
+  </div>
+);
