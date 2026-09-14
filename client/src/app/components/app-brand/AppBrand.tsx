3a693462a3375bd5786d21b9adf25495dc3a7c64
Salim <doshka23@gmail.com>
2026-09-14 10:50:33 +0200

    refined: app brand style

--- client/src/app/components/app-brand/AppBrand.tsx
+++ client/src/app/components/app-brand/AppBrand.tsx
@@ -4,14 +4,31 @@ import "./_app-brand.scss";
 export const AppBrand = () => {
     return (
         <header className="app-brand">
-            <div className="app-brand__mark">
-            <DiScrum />
+            <div className="app-brand__identity">
+
+                <div className="app-brand__mark">
+                    <span className="app-brand__mark-core">
+                        <DiScrum />
+                    </span>
+                </div>
+
+                <div className="app-brand__content">
+                    <div className="app-brand__name">
+                        <span>Flow</span>
+                        <span className="app-brand__name-accent">Board</span>
+                    </div>
+
+                    <div className="app-brand__meta">
+                        <span className="app-brand__meta-dot" />
+                        <span>Workspace platform</span>
+                    </div>
+                </div>
+
             </div>
 
-            <div className="app-brand__content">
-            <span className="app-brand__label">Plan. Build. Deliver.</span>
-            <strong className="app-brand__name">FlowBoard</strong>
+            <div className="app-brand__status">
+                <span className="app-brand__status-line" />
             </div>
         </header>
-    )
-}
+    );
+};
